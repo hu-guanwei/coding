72dd8c89169bd461cbd44f985c6953a7da96f357
Guanwei Hu <guanwei.woo@gmail.com>
2019-03-14 09:04:28 +0800

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,49 +1,41 @@
-from copy import deepcopy
+from random import randint
 
-class Solution(object):
-    def _partition(self, arr, l, r):
-        pivot = arr[l]
-        '''
-        * * * * * *
-        l         r
-        i k         j
-
-        closed interval
-        arr[l+1, i] < pivot
-        arr[k, j-1] to be determined
-        arr[j, r] >= pivot
-        '''
-
-        i = l
-        k = i + 1
-        j = r + 1
-
-        # stop when k = j
-        while k < j:
-            if arr[k] < pivot:
-                i += 1
-                k += 1
-            else:
-                arr[k], arr[j - 1] = arr[j - 1], arr[k]
-                j -= 1
-        arr[l], arr[i] = arr[i], arr[l]
-        return i
+def partition(arr, l, r):
+    '''
+	... * * * * * ...
+        l       r
+        i         j
+        arr[l, i] <= pivot
+        arr[i+1, j-1] to be considered
+        arr[j, r] > pivot
+    '''
+    i = l
+    j = r + 1
     
-    def _quick_sort(self, arr, l, r):
-        if l >= r:
-            return
-        p = self._partition(arr, l, r)
-        self._quick_sort(arr, l, p - 1)
-        self._quick_sort(arr, p + 1, r)
-
-    def quick_sort(self, arr):
-        arr2 = deepcopy(arr)
-        self._quick_sort(arr2, 0, len(arr)- 1)
-        return arr2
+    rd = randint(l, r)
+    arr[l], arr[rd] = arr[rd], arr[l]
+    pivot = arr[l]
     
-arr = [5, 6, 8, 9, 7, 10, 1, 2, 4, 3,]
-Solution()._partition(arr, 0, len(arr) - 1)
+    while i + 1  <= j - 1:
+        if arr[i + 1] <= pivot:
+            i += 1
+        else:
+            arr[i + 1], arr[j - 1] = arr[j - 1], arr[i + 1]
+            j -= 1
+    
+    arr[l], arr[i] = arr[i], arr[l]
+    
+    return i
+    
+def quick_sort(arr, l, r):
+    if l >= r:
+        return
+    
+    p = partition(arr, l, r)
+    quick_sort(arr, l, p - 1)
+    quick_sort(arr, p + 1, r)
+    
+arr = [randint(1, 100) for i in range(20)]
 print(arr)
-assert(Solution().quick_sort(arr) == [1,2,3,4,5,6,7,8,9,10])
-assert(Solution().quick_sort([38, 76, 99, -1, 100]) == [-1, 38, 76, 99,  100])
-assert(Solution().quick_sort([98, 97, 96, 95, 99, 94, 100]) == [94, 95, 96, 97, 98, 99, 100])
+quick_sort(arr, 0, len(arr) - 1)
+print(arr)
